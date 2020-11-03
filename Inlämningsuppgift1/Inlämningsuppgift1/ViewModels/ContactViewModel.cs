d92deb7b5648c7bb6a09049f91993e75207e2042
mustafaalispk <54665140+mustafaalispk@users.noreply.github.com>
2020-11-03 13:47:58 +0100

    Initial commit

---
+++ Inlämningsuppgift1/Inlämningsuppgift1/ViewModels/ContactViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Inlämningsuppgift1.ViewModels
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage ="Please enter your name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your email address")]
+        [EmailAddress(ErrorMessage = "You must enter a valid email address")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your message")]
+        [MaxLength(length:500, ErrorMessage = "Your message must be no longer then 500 characters")]
+        public string Message { get; set; }
+
+
+    }
+}
